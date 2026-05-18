d3c2be57a6a3df6249b315191972ca073421c7f3
PrimerNXT <primer.nxt@gmail.com>
2026-05-18 15:01:46 +0700

    fix: main.jsx import GarmentERPV5

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import GarmentERPV4 from './garment-erp-V4.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(<GarmentERPV4 />)
+import React from 'react' 
+import ReactDOM from 'react-dom/client' 
+import GarmentERPV5 from './App.jsx' 
+ReactDOM.createRoot(document.getElementById('root')).render(<GarmentERPV5 />) 
